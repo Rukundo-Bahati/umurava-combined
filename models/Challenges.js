ceb6a1fe0a3e651e23531f1533f6f2b6e5dc41f4
Rukundo-Bahati <rukundorca@gmail.com>
2025-01-29 21:55:29 +0200

    adding more requirements

--- models/Challenges.js
+++ models/Challenges.js
@@ -1,13 +1,18 @@
+// import { date } from "joi";
 import mongoose from "mongoose";
 
 const challengeSchema = new mongoose.Schema(
   {
-    image: { type: String },
+    title: { type: String },
+    money_prize: { type: String},
+    duration: { type: String},
+    email:  { type: String },
     skills: { type: String, required: true },
     level: { type: String, enum: ["Beginner", "Intermediate", "Advanced"], default: "Beginner" },
     timeline: { type: String, default: "7 days" },
     description: { type: String, default: "No description provided" },
-    instructions: { type: String, default: "Follow the guidelines and complete the challenge." },
+    brief: { type: String},
+    tasks: { type: String},
     status: { type: String, enum: ["Open", "Ongoing", "Completed"], default: "Open" }, 
   },
   { timestamps: true }
