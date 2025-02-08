171feaa37a767030ea47a3048e03526585351fc4
Rukundo-Bahati <rukundorca@gmail.com>
2025-02-08 19:57:26 +0200

    Added frontend and backend together

--- db/connection.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import c from "config";
-
-const CONNECTION = c.get("MONGO_URI") || "mongodb://localhost/Umurava";
-export default mongoose
-  .connect(CONNECTION)
-  .then(() => console.log(`Connected Successfully Umurava DB`))
-  .catch((error) => console.log(`${error} did not connect`));
